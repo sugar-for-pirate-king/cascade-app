5f925d05e16d708ba4bde02191ba3408cba7a36f
FauzanIchwan <fauzanichwan99@gmail.com>
2020-02-16 11:00:13 +0700

    add http module and lyrics page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },  {
+    path: 'lyrics',
+    loadChildren: () => import('./lyrics/lyrics.module').then( m => m.LyricsPageModule)
   }
+
 ];
 
 @NgModule({
---
+++ src/app/lyrics/lyrics.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lyrics',
+  templateUrl: './lyrics.page.html',
+  styleUrls: ['./lyrics.page.scss'],
+})
+export class LyricsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
